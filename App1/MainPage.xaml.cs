7ec18c9f277872b42eada4898ee83fd5f2530e29
aodpi <balan.valeriu@live.com>
2016-02-13 20:08:00 +0200

    PRORIV

---
+++ App1/MainPage.xaml.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using WordProcessor;
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace App1
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.Loaded += MainPage_Loaded;
+        }
+
+        IList<string> rz;
+        IList<string> _words;
+        int count = 0;
+        private void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            WordP wp = new WordP();
+            wp.Test(new int[11, 14]
+            {
+                {   0,  0, -1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0},
+                {   0, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 },
+                {   0,  0, -1,  0,  0,  0,  0,  0, -1,  0,  0,  0,  0, -1 },
+                {  -1, -1, -1, -1,  0,  0,  0, -1, -1, -1, -1,  0,  0, -1 },
+                {   0,  0, -1,  0,  0,  0,  0,  0, -1,  0,  0,  0,  0, -1 },
+                {   0,  0, -1,  0,  0,  0,  0,  0, -1,  0,  0,  0,  0, -1 },
+                {   0,  0, -1,  0,  0,  0,  0,  0, -1,  0,  0,  0,  0, -1 },
+                {   0,  0, -1,  0,  0,  0,  0,  0, -1,  0,  0,  0,  0, -1 },
+                {   0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, -1 },
+                {   0,  0, -1,  0,  0,  0,  0,  0, -1,  0,  0,  0,  0, -1 },
+                {  -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 },
+            }
+            , 11, 14);
+            //StorageFile sf = await StorageFile.GetFileFromApplicationUriAsync(new Uri("ms-appx:///wordlist.txt", UriKind.Absolute));
+            //_words = await FileIO.ReadLinesAsync(sf);
+            //WordP wp = new WordP(_words);
+            //string x = string.Empty;
+            //rz = wp.FindWordForIntersect("aam", 1, 4, 1);
+        }
+
+        public void DrawWord(string word)
+        {
+            if (Rebus.Children.Count > 0)
+            {
+                for (int i = 0; i < Rebus.Children.Count; i++)
+                {
+                    if (Rebus.Children[i].GetType()==typeof(TextBlock))
+                    {
+                        Rebus.Children.RemoveAt(i);
+                    }
+                }
+            }
+            ClearAll();
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                TextBlock txt = new TextBlock();
+                txt.Text = word[i].ToString();
+                txt.HorizontalAlignment = HorizontalAlignment.Center;
+                txt.VerticalAlignment = VerticalAlignment.Center;
+                txt.Tag = "llt";
+                Grid.SetRow(txt, 1);
+                Grid.SetColumn(txt, i);
+                Rebus.Children.Add(txt);
+            }
+            
+            WordP wp = new WordP(_words);
+            string x = wp.FindWordForIntersect(word, word.Length-1, 5, 1)[0];
+            for (int i = 0; i < x.Length; i++)
+            {
+                TextBlock txt = new TextBlock();
+                txt.Text = x[i].ToString();
+                txt.HorizontalAlignment = HorizontalAlignment.Center;
+                txt.VerticalAlignment = VerticalAlignment.Center;
+                Grid.SetRow(txt, i);
+                txt.Tag = "llt";
+                Grid.SetColumn(txt, 3);
+                Rebus.Children.Add(txt);
+            }
+        }
+
+        public void ClearAll()
+        {
+            Rebus.Children.Clear();
+        }
+        
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            DrawWord(rz[count++]);
+        }
+    }
+}
---
+++ WordProcessor/WordP.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Popups;
+
+namespace WordProcessor
+{
+    public class WordP
+    {
+        IList<string> _words;
+
+        public enum Orientation
+        {
+            Horizontal,
+            Vertical
+        }
+        public WordP()
+        {
+
+        }
+        public struct Rebus
+        {
+            public int xorigin, yorigin;
+            public Orientation orient;
+            public int length;
+            public int Id;
+            public List<Intersect> intersects;
+        }
+        public struct Intersect
+        {
+            public int xIntersect, yIntersect;
+            public int IntersectId;
+        }
+        public WordP(IList<string> Words)
+        {
+            this._words = Words;
+            char[,] x = new char[3, 3];
+            x[0, 0] = '1';
+        }
+
+        public void Test(int[,] matrix, int xmax, int ymax)
+        {
+            List<Rebus> arr = new List<Rebus>();
+            int i = 1, x, y;
+            Rebus temp;
+            //horizontal
+
+            for (x = 0; x < xmax; x++)
+            {
+                y = 0;
+                while (y < ymax - 1)
+                {
+                    if (matrix[x, y] != 0)
+                    {
+                        if (matrix[x, y + 1] != 0)
+                        {
+                            temp = new Rebus();
+                            temp.length = 0;
+                            temp.xorigin = x;
+                            temp.yorigin = y;
+                            temp.Id = i;
+                            temp.orient = Orientation.Horizontal;
+                            temp.intersects = new List<Intersect>();
+                            while (y < ymax && matrix[x, y] != 0)
+                            {
+                                temp.length++;
+                                matrix[x, y] = i;
+                                y++;
+                            }
+                            arr.Add(temp);
+                            i++;
+                        }
+                        else
+                            y++;
+                    }
+                    else y++;
+                }
+            }
+
+            //vertical
+            for (y = 0; y < ymax; y++)
+            {
+                x = 0;
+                while (x < xmax - 1)
+                {
+                    if (matrix[x, y] != 0)
+                    {
+                        if (matrix[x + 1, y] != 0)
+                        {
+                            temp = new Rebus();
+                            temp.length = 0;
+                            temp.xorigin = x;
+                            temp.yorigin = y;
+                            temp.Id = i;
+                            temp.orient = Orientation.Vertical;
+                            temp.intersects = new List<Intersect>();
+                            while (x < xmax && matrix[x, y] != 0)
+                            {
+                                temp.length++;
+                                if (matrix[x, y] != -1)
+                                {
+                                    arr[matrix[x, y] - 1].intersects.Add(new Intersect() { xIntersect = x, yIntersect = y, IntersectId = i });
+                                    temp.intersects.Add(new Intersect() { xIntersect = x, yIntersect = y, IntersectId = matrix[x, y] });
+                                }
+                                matrix[x, y] = i;
+                                x++;
+                            }
+                            arr.Add(temp);
+                            i++;
+                            continue;
+                        }
+                        else
+                            x++;
+                    }
+                    else
+                        x++;
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// Gaseste cuvantul potrivit pentru intersectarea cu cuvintul introdus. 
+        /// </summary>
+        /// <param name="firstword">Cuvintul deja prezent in rebus.</param>
+        /// <param name="intrsctpos">Index zero-based, al caracterului unde cuvintele se intersecteaza.</param>
+        /// <param name="requiredlength">Marimea cuvintului cautat care coincide cu spatiile in rebus.</param>
+        /// <param name="lengthbeforeint">Lungimea cuvintului cautat pina la intersectarea lor.</param>
+        /// <returns></returns>
+        public IList<string> FindWordForIntersect(string firstword,int intrsctpos,int requiredlength,int lengthbeforeint)
+        {
+            IList<string> foundwords = new List<string>();
+            foreach (string item in _words)
+            {
+                if (item.Length==requiredlength)
+                {
+                    char intrsctchar = firstword[intrsctpos];
+                    if (item.Contains(intrsctchar))
+                    {
+                        if (item.IndexOf(intrsctchar)-0==lengthbeforeint)
+                        {
+                            foundwords.Add(item);   
+                        }
+                    }
+                }
+            }
+            return foundwords;
+        }
+    }
+}
